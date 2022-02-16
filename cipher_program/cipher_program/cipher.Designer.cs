14cae72bbcf1de6332d42b5b779b33ded924837b
Pernell Robinson <55003474+Pernell98@users.noreply.github.com>
2022-02-16 03:36:44 -0500

    Add files via upload

---
+++ cipher_program/cipher_program/cipher.Designer.cs
@@ -0,0 +1,173 @@
+﻿
+namespace cipher_program
+{
+    partial class frmEncryptandDecrypt
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblValue = new System.Windows.Forms.Label();
+            this.lblEncrypt = new System.Windows.Forms.Label();
+            this.lblDecrypt = new System.Windows.Forms.Label();
+            this.txtValue = new System.Windows.Forms.TextBox();
+            this.txtEncrypt = new System.Windows.Forms.TextBox();
+            this.txtDecrypt = new System.Windows.Forms.TextBox();
+            this.btnEncrypt = new System.Windows.Forms.Button();
+            this.btnDecrypt = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblValue
+            // 
+            this.lblValue.AutoSize = true;
+            this.lblValue.Location = new System.Drawing.Point(31, 47);
+            this.lblValue.Name = "lblValue";
+            this.lblValue.Size = new System.Drawing.Size(48, 20);
+            this.lblValue.TabIndex = 0;
+            this.lblValue.Text = "Value:";
+            // 
+            // lblEncrypt
+            // 
+            this.lblEncrypt.AutoSize = true;
+            this.lblEncrypt.Location = new System.Drawing.Point(18, 84);
+            this.lblEncrypt.Name = "lblEncrypt";
+            this.lblEncrypt.Size = new System.Drawing.Size(61, 20);
+            this.lblEncrypt.TabIndex = 1;
+            this.lblEncrypt.Text = "Encrypt:";
+            // 
+            // lblDecrypt
+            // 
+            this.lblDecrypt.AutoSize = true;
+            this.lblDecrypt.Location = new System.Drawing.Point(15, 128);
+            this.lblDecrypt.Name = "lblDecrypt";
+            this.lblDecrypt.Size = new System.Drawing.Size(64, 20);
+            this.lblDecrypt.TabIndex = 2;
+            this.lblDecrypt.Text = "Decrypt:";
+            // 
+            // txtValue
+            // 
+            this.txtValue.Location = new System.Drawing.Point(94, 40);
+            this.txtValue.Name = "txtValue";
+            this.txtValue.Size = new System.Drawing.Size(333, 27);
+            this.txtValue.TabIndex = 3;
+            // 
+            // txtEncrypt
+            // 
+            this.txtEncrypt.Enabled = false;
+            this.txtEncrypt.Location = new System.Drawing.Point(94, 84);
+            this.txtEncrypt.Name = "txtEncrypt";
+            this.txtEncrypt.ReadOnly = true;
+            this.txtEncrypt.Size = new System.Drawing.Size(333, 27);
+            this.txtEncrypt.TabIndex = 4;
+            // 
+            // txtDecrypt
+            // 
+            this.txtDecrypt.Enabled = false;
+            this.txtDecrypt.Location = new System.Drawing.Point(94, 126);
+            this.txtDecrypt.Name = "txtDecrypt";
+            this.txtDecrypt.ReadOnly = true;
+            this.txtDecrypt.Size = new System.Drawing.Size(333, 27);
+            this.txtDecrypt.TabIndex = 5;
+            // 
+            // btnEncrypt
+            // 
+            this.btnEncrypt.Location = new System.Drawing.Point(136, 181);
+            this.btnEncrypt.Name = "btnEncrypt";
+            this.btnEncrypt.Size = new System.Drawing.Size(94, 29);
+            this.btnEncrypt.TabIndex = 6;
+            this.btnEncrypt.Text = "Encrypt";
+            this.btnEncrypt.UseVisualStyleBackColor = true;
+            this.btnEncrypt.Click += new System.EventHandler(this.btnEncrypt_Click);
+            // 
+            // btnDecrypt
+            // 
+            this.btnDecrypt.Location = new System.Drawing.Point(255, 181);
+            this.btnDecrypt.Name = "btnDecrypt";
+            this.btnDecrypt.Size = new System.Drawing.Size(94, 29);
+            this.btnDecrypt.TabIndex = 7;
+            this.btnDecrypt.Text = "Decrypt";
+            this.btnDecrypt.UseVisualStyleBackColor = true;
+            this.btnDecrypt.Click += new System.EventHandler(this.btnDecrypt_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(18, 181);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(94, 29);
+            this.btnClear.TabIndex = 8;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(368, 181);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(94, 29);
+            this.btnExit.TabIndex = 9;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // frmEncryptandDecrypt
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(489, 222);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnDecrypt);
+            this.Controls.Add(this.btnEncrypt);
+            this.Controls.Add(this.txtDecrypt);
+            this.Controls.Add(this.txtEncrypt);
+            this.Controls.Add(this.txtValue);
+            this.Controls.Add(this.lblDecrypt);
+            this.Controls.Add(this.lblEncrypt);
+            this.Controls.Add(this.lblValue);
+            this.Name = "frmEncryptandDecrypt";
+            this.Text = "Simple Cipher";
+            this.Load += new System.EventHandler(this.frmEncryptandDecrypt_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblValue;
+        private System.Windows.Forms.Label lblEncrypt;
+        private System.Windows.Forms.Label lblDecrypt;
+        private System.Windows.Forms.TextBox txtValue;
+        private System.Windows.Forms.TextBox txtEncrypt;
+        private System.Windows.Forms.TextBox txtDecrypt;
+        private System.Windows.Forms.Button btnEncrypt;
+        private System.Windows.Forms.Button btnDecrypt;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnExit;
+    }
+}
+
---
+++ cipher_program/cipher_program/cipher.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace cipher_program //Message-Digest Algorithm 5 (MD5)
+{
+    public partial class frmEncryptandDecrypt : Form
+    {
+        public frmEncryptandDecrypt()
+        {
+            InitializeComponent();
+        }
+
+        string hash = "f0xle@rn";
+        private void btnEncrypt_Click(object sender, EventArgs e)
+        {
+            byte[] data = UTF8Encoding.UTF8.GetBytes(txtValue.Text);
+            using (MD5CryptoServiceProvider md5 = new MD5CryptoServiceProvider())
+            {
+                byte[] keys = md5.ComputeHash(UTF8Encoding.UTF8.GetBytes(hash));
+                using (TripleDESCryptoServiceProvider tripDES = new TripleDESCryptoServiceProvider() { Key = keys, Mode = CipherMode.ECB, Padding = PaddingMode.PKCS7 }) //CipherMode.ECB considered a weakness
+                {
+                    ICryptoTransform transform = tripDES.CreateEncryptor();
+                    byte[] results = transform.TransformFinalBlock(data, 0, data.Length);
+                    txtEncrypt.Text = Convert.ToBase64String(results, 0, results.Length);
+                }
+            }
+        }
+
+        private void btnDecrypt_Click(object sender, EventArgs e)
+        {
+            byte[] data = Convert.FromBase64String(txtEncrypt.Text);
+            using (MD5CryptoServiceProvider md5 = new MD5CryptoServiceProvider())
+            {
+                byte[] keys = md5.ComputeHash(UTF8Encoding.UTF8.GetBytes(hash));
+                using (TripleDESCryptoServiceProvider tripDES = new TripleDESCryptoServiceProvider() { Key = keys, Mode = CipherMode.ECB, Padding = PaddingMode.PKCS7 }) //CipherMode.ECB considered a weakness
+                {
+                    ICryptoTransform transform = tripDES.CreateDecryptor();
+                    byte[] results = transform.TransformFinalBlock(data, 0, data.Length);
+                    txtDecrypt.Text = UTF8Encoding.UTF8.GetString(results);
+                }
+            }
+        }
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            txtValue.Clear();
+            txtEncrypt.Clear();
+            txtDecrypt.Clear();
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void frmEncryptandDecrypt_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
